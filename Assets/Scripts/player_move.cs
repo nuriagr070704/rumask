c505089cd43e6ce08a5d4b1767e1225e2d8a91d9
danihero21 <danihero21@hotmail.com>
2026-01-31 12:13:42 +0100

    personaje_update

--- Assets/Scripts/player_move.cs
+++ Assets/Scripts/player_move.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
-
-public class NewMonoBehaviourScript : MonoBehaviour
-{
+using System.Collections;
+using System.Collections.Generic;
+public class player_move : MonoBehaviour{ 
+    
+    private Rigidbody2D body2D;  
+    private float Vertical,Horizontal;
+    public float speed = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    void Start(){
+        body2D = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update(){
-        
-        
+        Horizontal = Input.GetAxisRaw("Horizontal");
+        Vertical = Input.GetAxisRaw("Vertical");
+    }
+    //Update para fisicas
+    private void FixedUpdate()
+    {
+        body2D.linearVelocity= new Vector2(speed*Horizontal,speed*Vertical);
     }
 }
